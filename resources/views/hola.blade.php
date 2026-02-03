ef4974d764e4d662eb853af84b959ea99aad66a2
SaulDV <felix21soc@gmail.com>
2026-02-03 12:11:20 -0500

    test de deploy

--- resources/views/hola.blade.php
+++ resources/views/hola.blade.php
@@ -7,6 +7,11 @@
 
 <body>
     <h1>¡Laravel funciona! 🚀</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maxime, beatae enim inventore non odio soluta,
+        quia fuga repellendus quibusdam quam! Exercitationem voluptatem earum minima, dicta provident architecto tempora
+        sed.
+    </p>
 </body>
 
 </html>
